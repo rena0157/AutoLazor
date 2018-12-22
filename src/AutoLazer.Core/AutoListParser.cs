ecba70f56a82b2f55757cb6c4c67945ec8dd77a6
Adam Renaud <adamf.renaud@gmail.com>
2018-12-22 11:44:15 -0500

    Making the AutoListParser Class with GetObjects Function

--- src/AutoLazer.Core/AutoListParser.cs
+++ src/AutoLazer.Core/AutoListParser.cs
@@ -7,13 +7,25 @@ namespace AutoLazer.Core
     public static class AutoListParser
     {
         /// <summary>
-        /// Get Object From the AutoList Text
+        /// Get objects from a string using Regular Expressions
         /// </summary>
-        /// <param name="pattern"></param>
-        /// <returns></returns>
+        /// <param name="inputText">The input text</param>
+        /// <param name="pattern">The pattern that is used, note that the
+        /// Captured objects must be part of a group called 'target'</param>
+        /// <typeparam name="T">The desired return type</typeparam>
+        /// <returns>A list containing the objects that were taken from the string</returns>
         public static List<T> GetObjects<T>(string inputText, string pattern)
         {
-            return new List<T>();
+            var regex = new Regex(pattern);
+            var matchCollection = regex.Matches(inputText);
+            var returnList = new List<T>();
+
+            foreach (Match match in matchCollection)
+                returnList.Add((T)Convert
+                    .ChangeType(match.Groups["target"]
+                    .Value, typeof(T)));
+
+            return returnList;
         }
     }
 }
