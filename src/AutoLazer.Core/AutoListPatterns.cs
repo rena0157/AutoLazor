aabf47b16a01ca73e867e666aea624888e607a25
Adam Renaud <adamf.renaud@gmail.com>
2018-12-22 14:23:39 -0500

    Removed the lowercase a because it would also capture the area of polylines

--- src/AutoLazer.Core/AutoListPatterns.cs
+++ src/AutoLazer.Core/AutoListPatterns.cs
@@ -6,7 +6,7 @@ namespace AutoLazer.Core
     {
         public const string LinesLengthPattern = @"[L,l]ength\s+=?\s*(?<target>\d+\.?\d*)";
 
-        public const string HatchAreaPattern = @"[A,a]rea\s*(?<target>\d+\.?\d*)";
+        public const string HatchAreaPattern = @"[A]rea\s*(?<target>\d+\.?\d*)";
 
         public const string TextPattern = @"([T,t]ext|Contents:)\s*(?<target>.*)";
     }
