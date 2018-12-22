b368feddf6b16f144251c0493549673e71efe71a
Adam Renaud <adamf.renaud@gmail.com>
2018-12-22 12:51:38 -0500

    Adding the GetBlocks Logic

--- src/AutoLazer.Core/AutoListParser.cs
+++ src/AutoLazer.Core/AutoListParser.cs
@@ -27,5 +27,67 @@ namespace AutoLazer.Core
 
             return returnList;
         }
+
+        public static List<Block> GetBlocks(string inputText)
+        {
+            var textObjects = GetObjects<string>(inputText, AutoListPatterns.TextPattern);
+            var lengths = GetObjects<double>(inputText, AutoListPatterns.LinesLengthPattern);
+            var areas = GetObjects<double>(inputText, AutoListPatterns.HatchAreaPattern);
+
+            const string orderValidationPattern = @"(LINE|LWPOLYLINE|HATCH|TEXT|MTEXT)";
+            var matches = Regex.Matches(inputText, orderValidationPattern);
+
+            var textIndex = 0;
+            var lineIndex = 0;
+            var areaIndex = 0;
+
+            string currentText = null;
+            double currentLength = 0;
+            double currentArea = 0;
+            var blocks = new List<Block>(textObjects.Capacity);
+
+            for ( var matchIndex = 0; matchIndex < matches.Count; ++matchIndex )
+            {
+                var currentMatch = matches[matchIndex];
+
+                // Get the initial block ID
+                if ( currentText == null && ( currentMatch.Value == "TEXT" || currentMatch.Value == "MTEXT" ) )
+                {
+                    currentText = textObjects[textIndex++];
+                    continue;
+                }
+
+                // Add length of a line and polyline to the total length
+                if ( currentMatch.Value == "LWPOLYLINE" || currentMatch.Value == "LINE" )
+                {
+                    currentLength += lengths[lineIndex++];
+                    continue;
+                }
+
+                // If the current item is a hatch then add the area of the hatch
+                // to the list
+                if ( currentMatch.Value == "HATCH" )
+                {
+                    currentArea += areas[areaIndex++];
+                    continue;
+                }
+
+                // Build the block and place it into the list
+                if ( currentText != null && ( currentMatch.Value == "TEXT" || currentMatch.Value == "MTEXT" ) )
+                {
+                    blocks.Add(new Block(currentText, currentLength, currentArea));
+                    currentText = textObjects[textIndex++];
+                    currentLength = 0;
+                    currentArea = 0;
+                }
+            }
+
+            // Build the final block
+            if ( blocks.Count < textObjects.Count )
+                blocks.Add(new Block(currentText, currentLength, currentArea));
+
+            return blocks;
+        }
+
     }
 }
