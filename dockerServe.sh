12669dba645566d613e2c29b5f7458a26bfe74b3
Adam Renaud <adamf.renaud@gmail.com>
2018-12-23 13:25:41 -0500

    Removing git

--- dockerServe.sh
+++ dockerServe.sh
@@ -1,12 +1,4 @@
 
-# Simple bash script that sets up the site on the server
-$githubRepo = "https://github.com/rena0157/AutoLazor.git"
-echo "Clone from repo"
-git clone $githubRepo
-
-# go into the directory
-cd AutoLazor/
-
 echo "Building Docker image"
 
 # Build the docker image
