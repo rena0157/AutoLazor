66ac8960cfef90134fef5eebb3ebbdee527c666f
Adam Renaud <adamf.renaud@gmail.com>
2018-12-23 13:24:28 -0500

    adding user messages

--- dockerServe.sh
+++ dockerServe.sh
@@ -7,8 +7,11 @@ git clone $githubRepo
 # go into the directory
 cd AutoLazor/
 
+echo "Building Docker image"
+
 # Build the docker image
 docker build . -t autolazer
 
+echo "Runner Docker"
 # run the docker image
 docker run -d -p 80:80 autolazer --name autolazer
