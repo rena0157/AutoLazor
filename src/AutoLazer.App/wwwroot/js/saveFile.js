5f902290d7b4888166ff9dfa75f629ec84a6ef72
Adam Renaud <adamf.renaud@gmail.com>
2018-12-23 09:11:57 -0500

    Adding comments

--- src/AutoLazer.App/wwwroot/js/saveFile.js
+++ src/AutoLazer.App/wwwroot/js/saveFile.js
@@ -1,3 +1,6 @@
+/**
+ * Create a link and prompt the user to save the file.
+ */
 function saveFile(filename, bytesBase64) {
     var link = document.createElement('a');
     link.download = filename;
